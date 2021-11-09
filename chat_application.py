3e1cfba7e1a13a5760123878d3960eabb7d141c0
Laura Dobie <moving.toyshop@gmail.com>
2021-11-09 21:53:39 +0000

    Two way messaging working, to a degree.

--- chat_application.py
+++ chat_application.py
@@ -1,10 +1,11 @@
 import socket
 
 # Ask if the user wants to be a client or a server.
-# If a server, ask for the IP address and port.
-# Set up a server using this IP address and port
-# If the user selects client, connect to server as a client
-# Set up a loop to get a message and allow the user to enter a reply, until the user types end.
+# Ask for a port number.
+# Set up a server using this port
+# Connect the client
+# If the user selected server, set up a loop to get a message from the client and allow the user to enter a reply to send from the server, until the user types end.
+# If the user selected client, set up a loop to get a message from the server and allow the user to enter a reply to send from the client, until the user types end.
 def set_up_server(IP_address, port):
     port_int = int(port)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,6 +16,8 @@ def set_up_server(IP_address, port):
 
 def accept_client_connection(server_socket):
     connection_socket, address = server_socket.accept()
+    
+    return connection_socket
 
 def client_connect(IP_address, port):
     print("Connecting...")
@@ -54,16 +57,51 @@ if selection.lower() == "server":
     server_socket = set_up_server("0.0.0.0", port)
     print("Waiting for client to connect")
     client = client_connect("127.0.0.1", port)
-    accept_client_connection(server_socket)
+    connection_socket = accept_client_connection(server_socket)
     print("A client has connected")
-    message = input("Enter your message >")
+    end = False
+    while end == False:
+        print("Waiting for client message...")
+        send_text(client, "Hello, I'm a client!")
+        client_message = next(get_text(connection_socket))
+        print(client_message)
+        reply = input("Enter your message. Type end to exit. >")
+        if reply.lower() == "end":
+            end == True
+            break
+        send_text(connection_socket, reply)
+        print("Your message has been sent.")
+        server_message = next(get_text(client))
+        print("Your message has been received: " + server_message)
+
+    server_socket.close()
+    client.close()
+
 
 
 if selection.lower() == "client":
-    server_socket = set_up_server("0.0.0.0", 8082)
+    port = input("Enter a port number>")
+    server_socket = set_up_server("0.0.0.0", port)
     print("Setting up server...")
-    client = client_connect("127.0.0.1", 8082)
-    accept_client_connection(server_socket)
+    client = client_connect("127.0.0.1", port)
+    connection_socket = accept_client_connection(server_socket)
     print("You have connected to the server")
-    message = input("Enter your message >")
+    end = False
+    while end == False:
+        print("Waiting for server message...")
+        send_text(connection_socket, "Hello, I'm a server!")
+        server_message = next(get_text(client))
+        print(server_message)
+        reply = input("Enter your message. Type end to exit. >")
+        if reply.lower() == "end":
+            end == True
+            break
+        send_text(client, reply)
+        print("Your message has been sent")
+        client_message = next(get_text(connection_socket))
+        print("Your message to the server has been received: " + client_message)
+        
+
+    server_socket.close()
+    client.close()
 
