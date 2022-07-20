78b0d4a7fb2a1a31dfc6661890c0936fe138a2cf
Laura Dobie <moving.toyshop@gmail.com>
2022-07-20 22:37:30 +0100

    Started networked quiz. Can send question, receive answer, give feedback

---
+++ client_starter.py
@@ -0,0 +1,39 @@
+import socket
+# Import the functions from the networking tools module
+from fl_networking_tools import get_binary, send_binary
+
+'''
+Responses
+LIST YOUR RESPONSE CODES HERE
+1 - Question
+2 - Answer
+3 - Score
+4 - End
+'''
+team_name = input("Enter a team name > ")
+print("Greetings, " + team_name + ", welcome to the quiz!")
+
+ip_address = input("Enter the IP address that the quiz is running on > ")
+print("Connecting to " + ip_address + "...")
+
+# A flag used to control the quiz loop.
+playing = True
+
+quiz_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+quiz_server.connect((ip_address, 2065))
+
+# Sending a command to the server.
+send_binary(quiz_server, ["QUES", ""])
+
+while playing:
+    # The get_binary function returns a list of messages - loop over them
+    for response in get_binary(quiz_server):
+        # response is the command/response tuple - response[0] is the code
+        if response[0] == 1: # The question response
+            # Display it to the user.
+            print(response[1])
+            answer = input("Enter your answer > ")
+            send_binary(quiz_server, ["ANS", answer])
+        if response[0] == 2:
+            print(response[1])
---
+++ server_starter.py
@@ -0,0 +1,43 @@
+# The socket server library is a more powerful module for handling sockets, it will help you set up and manage multiple clients in the next step
+import socketserver
+from collections import namedtuple
+from fl_networking_tools import get_binary, send_binary
+
+'''
+Commands:
+PLACE YOUR COMMANDS HERE
+QUES - question command
+ANS - answer command
+SCO - score command
+END - end command
+'''
+
+# Named tuples are extensions of the tuple structure, with contents you can refer to by name. In this case, the question will be held in a variable named q and the answer in answer.
+# This is just the set up of the question - it will be sent to the client using the send_binary function when a request is made.
+Question = namedtuple('Question', ['q', 'answer'])
+
+q1 = Question("Who won the Booker Prize in 2020?", "Douglas Stuart")
+q2 = Question("Who wrote Circe?", "Madeleine Miller")
+
+# The socketserver module uses 'Handlers' to interact with connections. When a client connects a version of this class is made to handle it.
+class QuizGame(socketserver.BaseRequestHandler):
+    # The handle method is what actually handles the connection
+    def handle(self):
+        #Retrieve Command
+        for command in get_binary(self.request):
+            score = 0
+            if command[0] == "QUES":
+                #Send question
+                send_binary(self.request, (1, q1.q))
+            if command[0] == "ANS":
+                if command[1] == "Douglas Stuart":
+                    send_binary(self.request, (2, "Correct!"))
+                else:
+                    send_binary(self.request, (2, "Incorrect, the answer is " + q1.answer))
+        #Your server code goes here
+
+
+# Open the quiz server and bind it to a port - creating a socket
+# This works similarly to the sockets you used before, but you have to give it both an address pair (IP and port) and a handler for the server.
+quiz_server = socketserver.TCPServer(('127.0.0.1', 2065), QuizGame)
+quiz_server.serve_forever()
